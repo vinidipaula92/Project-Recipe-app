1ff3a9f0eb760a72bc77c1b0c3728bf616270eb3
imarmendes <imarmendes@gmail.com>
2022-04-26 18:54:20 -0300

    :constrution: biuld: update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Login() {
   return (
-    <p>Login</p>
+    <div>
+      <p>Login</p>
+      <Link to="foods">
+        Va para foods.
+      </Link>
+    </div>
   );
 }
 
