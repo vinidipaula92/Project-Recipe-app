1ca1564dd4e0b340ca6509033d243ebc5ba9d28e
imarmendes <imarmendes@gmail.com>
2022-04-26 18:01:44 -0300

    :bookmark: Tela de footer feita mas não testada

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Footer() {
   return (
     <div>
       <footer data-testid="footer">
-        <img
-          data-testid="food-bottom-btn"
-          src="src/images/mealIcon.svg"
-          alt="meal"
-        />
-        <img
-          data-testid="drinks-bottom-btn"
-          src="src/images/drinkIcon.svg"
-          alt="drink"
-        />
-        <img
-          data-testid="explore-bottom-btn"
-          src="src/images/exploreIcon.svg"
-          alt="explore"
-        />
+        <Link to="/foods">
+          <img
+            data-testid="food-bottom-btn"
+            src="src/images/mealIcon.svg"
+            alt="meal"
+          />
+        </Link>
+        <Link to="/drinks">
+          <img
+            data-testid="drinks-bottom-btn"
+            src="src/images/drinkIcon.svg"
+            alt="drink"
+          />
+        </Link>
+        <Link to="/explore">
+
+          <img
+            data-testid="explore-bottom-btn"
+            src="src/images/exploreIcon.svg"
+            alt="explore"
+          />
+        </Link>
       </footer>
     </div>
   );
