c9876e7afdb11fa8c3f3bb06dc2252a9d1b0d079
imarmendes <imarmendes@gmail.com>
2022-04-26 17:41:05 -0300

    :construction: build: Tela de footer parcial

---
+++ src/pages/ExploreDrinks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+export default function ExploreDrinks() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/ExploreMealByIngredient.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+export default function ExploreMealByIngredient() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
