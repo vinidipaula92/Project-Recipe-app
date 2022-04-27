29becd7fabfc66770456b3c95fd0672bac91e1a3
vicdev <vtavares.tur@gmail.com>
2022-04-27 14:38:10 -0300

    implementação de tela de login - req 02 a 08

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom';
 
-function Login() {
+export default function Login() {
+  const history = useHistory();
+  const [user, setUser] = userState({
+    email: '',
+    password: '',
+  });
+
+  const [disabled, setDisabled] = userState(true);
+
+  const enterButtonValidation = () => {
+    const emailRegex = /\S +@\S+\.\S+/;
+    const numberSix = 6;
+    if (emailRegex.test(user.email) && (user.password.length > numberSix)) {
+      setDisabled(false);
+    } else { setDisabled(true); }
+  };
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUser({ ...user, [name]: value });
+    enterButtonValidation();
+  };
+
+  const handleClick = () => {
+    localStorage.setItem(user, { email: value });
+    history.push('/foods');
+  };
+
   return (
     <div>
       <p>Login</p>
-      <Link to="foods">
-        Va para foods.
-      </Link>
+      <form>
+        <input
+          className="emailInput"
+          type="text"
+          name="email"
+          data-testid="email-input"
+          onChange={ handleChange }
+        />
+        <input
+          className="passwordInput"
+          type="password"
+          name="password"
+          data-testid="password-input"
+          onChange={ handleChange }
+        />
+        <button
+          data-testid="login-submit-btn"
+          type="button"
+          disabled={ disabled }
+          onClick={ handleClick }
+        >
+          Entrar
+        </button>
+      </form>
     </div>
   );
 }
-
-export default Login;
