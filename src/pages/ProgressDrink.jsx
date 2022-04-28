5dd98af2810e3b2962afd2d8080b396c8944e28a
vinidipaula <vinidipaula92@gmail.com>
2022-04-28 18:26:49 -0300

    Requisito 47 estrurado

--- src/pages/ProgressDrink.jsx
+++ src/pages/ProgressDrink.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
 
 export default function ProgressDrink() {
+  const [add, setAdd] = useState(false);
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    if (count === 0) {
+      setAdd(true);
+      setCount(1);
+    } else {
+      setAdd(false);
+      setCount(0);
+    }
+  };
   return (
     <div>
-      Progresso da bebida
+      <h1>Progress Drink</h1>
+      <h3 data-testid="recipe-title">Titulo</h3>
+      <img
+        src="https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg"
+        alt="hamburger"
+      />
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share
+
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favorite
+
+      </button>
+      <p data-testid="recipe-category">Category</p>
+      <label
+        htmlFor="ingredients"
+      >
+        Ingredients:
+        <input
+          type="checkbox"
+          id="ingredients"
+          data-testid="ingredient-step"
+          className={ add ? 'ingredient-step' : null }
+          onClick={ handleClick }
+        />
+      </label>
+      <p data-testid="instructions">Intructions</p>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Finish
+
+      </button>
     </div>
   );
 }
--- src/pages/ProgressFood.jsx
+++ src/pages/ProgressFood.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../css/progress.css';
 
 export default function ProgressFood() {
+  const [add, setAdd] = useState(false);
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    if (count === 0) {
+      setAdd(true);
+      setCount(1);
+    } else {
+      setAdd(false);
+      setCount(0);
+    }
+  };
   return (
     <div>
-      Progresso da comida
+      <h1>Progress Food</h1>
+      <h3 data-testid="recipe-title">Titulo</h3>
+      <img
+        src="https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg"
+        alt="hamburger"
+      />
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share
+
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favorite
+
+      </button>
+      <p data-testid="recipe-category">Category</p>
+      <label
+        htmlFor="ingredients"
+      >
+        Ingredients:
+        <input
+          type="checkbox"
+          id="ingredients"
+          data-testid="ingredient-step"
+          className={ add ? 'ingredient-step' : null }
+          onClick={ handleClick }
+        />
+      </label>
+      <p data-testid="instructions">Intructions</p>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Finish
+
+      </button>
     </div>
   );
 }
