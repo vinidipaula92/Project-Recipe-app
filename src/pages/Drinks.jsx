bb8d8916491f8ef67e5e0b1554e9e677135aa6c9
imarmendes <imarmendes@gmail.com>
2022-04-28 12:08:45 -0300

    Requisito 17 e 18, testado e aprovado

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Searchheader from '../components/SearchHeader';
-// import { NUMBER_ZERO } from '../services/consts';
+import { NUMBER_ELEVEN } from '../services/consts';
 
 export default function Drinks() {
-  const dataDrink = useSelector((state) => state.dataReducer.dataDrink);
-
-  if (dataDrink.drinks !== undefined && dataDrink.drinks.length === NUMBER_ZERO) {
-    global.alert('Sorry, we haven\'t found any recipes for these filters.');
-  }
-  console.log(dataDrink);
+  const { drinks } = useSelector((state) => state.dataReducer.dataDrink);
   return (
     <div>
       <Header />
       <span data-testid="page-title">Drinks</span>
       <Searchheader />
+      {
+        drinks && drinks.map((drink, index) => (
+          index <= NUMBER_ELEVEN && (
+            <div data-testid={ `${index}-recipe-card` } key={ drink.idDrink }>
+              <Link to={ `/drinks/${drink.idMeal}` }>
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ drink.strDrinkThumb }
+                  alt="recipes cards"
+                />
+                <p data-testid={ `${index}-card-name` }>{drink.strDrink}</p>
+              </Link>
+            </div>
+          )))
+      }
       <Footer />
     </div>
   );
