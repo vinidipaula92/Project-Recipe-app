fe38805c7a15f1293b4edf3602377d9acace26ce
Pedrin <pedro515vieira@gmail.com>
2022-04-28 16:55:57 -0300

    :heavy_check_mark: feat: Requisito 28

--- src/components/CategoriesRender.jsx
+++ src/components/CategoriesRender.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import { requestDrinksCategories, requestMealsCategories } from '../services/apiRequest';
+import { requestDrinksCategories,
+  requestMealsCategories,
+  requestMealsByCategory,
+  requestDrinksByCategories } from '../services/apiRequest';
 import { NUMBER_FIVE } from '../services/consts';
+import { saveDataDrink, saveDataFood } from '../redux/actions';
 
 export default function CategoriesRender() {
   const { pathname } = useLocation();
   const [categoriesList, setCategoriesList] = useState([]);
+  const dispatch = useDispatch();
 
   async function provideCategories() {
     if (pathname === '/drinks') {
@@ -17,6 +23,17 @@ export default function CategoriesRender() {
       const mealsCategories = await requestMealsCategories();
       const { meals } = mealsCategories;
       setCategoriesList(meals);
+    }
+  }
+
+  async function filterCategories({ target }) {
+    const { value } = target;
+    if (pathname === '/drinks') {
+      const drinksByCategorie = await requestDrinksByCategories(value);
+      dispatch(saveDataDrink(drinksByCategorie));
+    } else if (pathname === '/foods') {
+      const mealsByCategorie = await requestMealsByCategory(value);
+      dispatch(saveDataFood(mealsByCategorie));
     }
   }
 
@@ -34,6 +51,8 @@ export default function CategoriesRender() {
               type="button"
               data-testid={ `${element.strCategory}-category-filter` }
               key={ element.strCategory }
+              value={ element.strCategory }
+              onClick={ filterCategories }
             >
               {element.strCategory}
             </button>)
