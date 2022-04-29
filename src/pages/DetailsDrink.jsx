4ad26b9dcaf297436fd39cca18b868ae17680904
imarmendes <imarmendes@gmail.com>
2022-04-29 20:36:40 -0300

    Comite para atualizar

--- src/pages/DetailsDrink.jsx
+++ src/pages/DetailsDrink.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { requestDrinkRecipeById } from '../services/apiRequest';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import Slider from 'react-slick';
+import { useClipboard } from 'use-clipboard-copy';
+import { saveDataFood } from '../redux/actions';
+import { requestDrinkRecipeById, requestMeal } from '../services/apiRequest';
+import { NUMBER_SIX } from '../services/consts';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+import '../css/footer.css';
 
 export default function DetailsDrink() {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const clipboard = useClipboard();
   const [drinkRecipe, setDrinkRecipe] = useState({});
   const [loading, setLoading] = useState(true);
+  const [share, setShare] = useState(true);
+  const [favorite, setFavorite] = useState(true);
 
   const getRecipeById = async () => {
     const { drinks } = await requestDrinkRecipeById(id);
@@ -13,16 +26,40 @@ export default function DetailsDrink() {
     setLoading(false);
   };
 
+  async function askApi() {
+    const mealsList = await requestMeal();
+    dispatch(saveDataFood(mealsList));
+  }
+
   useEffect(() => {
     getRecipeById();
+    askApi();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const { meals } = useSelector((state) => state.dataReducer.dataFood);
   const ingredients = Object.keys(drinkRecipe)
     .filter((key) => key.includes('strIngredient'));
-  const measure = Object.keys(drinkRecipe).filter((key) => key.includes('strMeasure'));
+  const measure = Object.keys(drinkRecipe)
+    .filter((key) => key.includes('strMeasure'));
 
-  console.log(drinkRecipe);
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
+
+  const handleCopy = () => {
+    clipboard.copy(`http://localhost:3000/drinks/${drinkRecipe.idDrink}`);
+    setShare(false);
+  };
+
+  const handleChangeFavorite = () => {
+    if (favorite) return setFavorite(false);
+    return setFavorite(true);
+  };
 
   return (
     <div>
@@ -35,9 +72,7 @@ export default function DetailsDrink() {
               alt={ drinkRecipe.strDrink }
             />
             <h1 data-testid="recipe-title">{drinkRecipe.strDrink}</h1>
-            <button type="button" data-testid="share-btn">Share</button>
-            <button type="button" data-testid="favorite-btn">Favorite</button>
-            <p data-testid="recipe-category">{drinkRecipe.strCategory}</p>
+            <p data-testid="recipe-category">{drinkRecipe.strAlcoholic}</p>
             {
               ingredients.map((ingredient, index) => (drinkRecipe[ingredient]
                 && (
@@ -61,14 +96,83 @@ export default function DetailsDrink() {
               />
               Your browser does not support the video tag.
             </video>
-
-            <p data-testid={ `${0}-recomendation-card` }>Card recomedações faze depois</p>
-
-            <button type="button" data-testid="start-recipe-btn">Start</button>
+            <Slider { ...settings }>
+              {
+                meals && meals.map((meal, index) => (
+                  index < NUMBER_SIX && (
+                    <div
+                      data-testid={ `${index}-recomendation-card` }
+                      key={ meal.idMeal }
+                    >
+                      <Link to={ `/foods/${meal.idMeal}` }>
+                        <img
+                          data-testid={ `${index}-card-img` }
+                          src={ meal.strMealThumb }
+                          alt="recipes cards"
+                        />
+                        <p
+                          data-testid={ `${index}-recomendation-title` }
+                        >
+                          {meal.strMeal}
+                        </p>
+                      </Link>
+                    </div>
+                  )))
+              }
+            </Slider>
+            <div>
+              <button
+                type="button"
+                data-testid="share-btn"
+                onClick={ handleCopy }
+              >
+                { share ? (
+                  <img
+                    src={ shareIcon }
+                    alt="compartilhar"
+                  />
+                ) : (
+                  <p>Link copied!</p>
+                ) }
+              </button>
+              <button
+                type="button"
+                onClick={ handleChangeFavorite }
+              >
+                {favorite
+                  ? (
+                    <img
+                      data-testid="favorite-btn"
+                      src={ whiteHeartIcon }
+                      alt="favoritar"
+                    />
+                  )
+                  : (
+                    <img
+                      data-testid="favorite-btn"
+                      src={ blackHeartIcon }
+                      alt="favoritar"
+                    />
+                  )}
+              </button>
+            </div>
+            <p>
+              a
+              a
+              a
+            </p>
+            <Link to={ `/drinks/${drinkRecipe.idDrink}/in-progress` }>
+              <button
+                className="footer-fixed"
+                type="button"
+                data-testid="start-recipe-btn"
+              >
+                Start
+              </button>
+            </Link>
           </div>
         )
       }
-
     </div>
   );
 }
--- src/pages/DetailsFood.jsx
+++ src/pages/DetailsFood.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { requestFoodRecipeById } from '../services/apiRequest';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import Slider from 'react-slick';
+import { useClipboard } from 'use-clipboard-copy';
+import { saveDataDrink } from '../redux/actions';
+import { requestDrinks, requestFoodRecipeById } from '../services/apiRequest';
+import { NUMBER_SIX } from '../services/consts';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+import '../css/footer.css';
 
 export default function DetailsFood() {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const clipboard = useClipboard();
   const [recipe, setRecipe] = useState({});
   const [loading, setLoading] = useState(true);
+  const [share, setShare] = useState(true);
+  const [favorite, setFavorite] = useState(true);
 
   const getRecipeById = async () => {
     const { meals } = await requestFoodRecipeById(id);
@@ -13,14 +26,41 @@ export default function DetailsFood() {
     setLoading(false);
   };
 
+  async function askApi() {
+    const drinksList = await requestDrinks();
+    dispatch(saveDataDrink(drinksList));
+  }
+
   useEffect(() => {
     getRecipeById();
+    askApi();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const ingredients = Object.keys(recipe).filter((key) => key.includes('strIngredient'));
-  const measure = Object.keys(recipe).filter((key) => key.includes('strMeasure'));
-  console.log(ingredients, measure);
+  const { drinks } = useSelector((state) => state.dataReducer.dataDrink);
+  const ingredients = Object.keys(recipe)
+    .filter((key) => key.includes('strIngredient'));
+  const measure = Object.keys(recipe)
+    .filter((key) => key.includes('strMeasure'));
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
+
+  const handleCopy = () => {
+    clipboard.copy(`http://localhost:3000/foods/${recipe.idMeal}`);
+    setShare(false);
+  };
+
+  const handleChangeFavorite = () => {
+    if (favorite) return setFavorite(false);
+    return setFavorite(true);
+  };
+
   return (
     <div>
       {
@@ -32,8 +72,6 @@ export default function DetailsFood() {
               alt={ recipe.strMeal }
             />
             <h1 data-testid="recipe-title">{recipe.strMeal}</h1>
-            <button type="button" data-testid="share-btn">Share</button>
-            <button type="button" data-testid="favorite-btn">Favorite</button>
             <p data-testid="recipe-category">{recipe.strCategory}</p>
             {
               ingredients.map((ingredient, index) => (recipe[ingredient] !== ''
@@ -58,14 +96,83 @@ export default function DetailsFood() {
               />
               Your browser does not support the video tag.
             </video>
-
-            <p data-testid={ `${0}-recomendation-card` }>Card recomedações faze depois</p>
-
-            <button type="button" data-testid="start-recipe-btn">Start</button>
+            <Slider { ...settings }>
+              {
+                drinks && drinks.map((drink, index) => (
+                  index < NUMBER_SIX && (
+                    <div
+                      data-testid={ `${index}-recomendation-card` }
+                      key={ drink.idDrink }
+                    >
+                      <Link to={ `/drinks/${drink.idDrink}` }>
+                        <img
+                          data-testid={ `${index}-card-img` }
+                          src={ drink.strDrinkThumb }
+                          alt="recipes cards"
+                        />
+                        <p
+                          data-testid={ `${index}-recomendation-title` }
+                        >
+                          {drink.strDrink}
+                        </p>
+                      </Link>
+                    </div>
+                  )))
+              }
+            </Slider>
+            <div>
+              <button
+                type="button"
+                data-testid="share-btn"
+                onClick={ handleCopy }
+              >
+                { share ? (
+                  <img
+                    src={ shareIcon }
+                    alt="compartilhar"
+                  />
+                ) : (
+                  <p>Link copied!</p>
+                ) }
+              </button>
+              <button
+                type="button"
+                onClick={ handleChangeFavorite }
+              >
+                {favorite
+                  ? (
+                    <img
+                      data-testid="favorite-btn"
+                      src={ whiteHeartIcon }
+                      alt="favoritar"
+                    />
+                  )
+                  : (
+                    <img
+                      data-testid="favorite-btn"
+                      src={ blackHeartIcon }
+                      alt="favoritar"
+                    />
+                  )}
+              </button>
+            </div>
+            <p>
+              a
+              a
+              a
+            </p>
+            <Link to={ `/foods/${recipe.idMeal}/in-progress` }>
+              <button
+                className="footer-fixed"
+                type="button"
+                data-testid="start-recipe-btn"
+              >
+                Start
+              </button>
+            </Link>
           </div>
         )
       }
-
     </div>
   );
 }
