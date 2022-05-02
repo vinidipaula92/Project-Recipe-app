59895546b09ebcd0318039ef60d93943f5eb29b8
vinidipaula <vinidipaula92@gmail.com>
2022-05-02 19:07:21 -0300

    :chore: Requisito 76 finalizado

--- src/pages/ExploreDrinkByIngredient.jsx
+++ src/pages/ExploreDrinkByIngredient.jsx
@@ -4,31 +4,31 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { saveDataDrink } from '../redux/actions';
-import { requestDrinks } from '../services/apiRequest';
+import { requestExploreIngredientDrink } from '../services/apiRequest';
 import { NUMBER_ELEVEN } from '../services/consts';
 
 export default function ExploreDrinkByIngredient() {
   const [loading, setLoading] = useState(true);
   const { drinks } = useSelector((state) => state.dataReducer.dataDrink);
   const dispatch = useDispatch();
-  const [drink, setDrink] = useState({});
+  // const [ingredient, setIngredient] = useState([]);
 
   async function askApi() {
-    const drinksList = await requestDrinks();
+    const drinksList = await requestExploreIngredientDrink();
     dispatch(saveDataDrink(drinksList));
     setLoading(false);
-    setDrink(drinksList.drinks[0]);
+    // setIngredient(drinksList);
   }
+
   useEffect(() => {
     askApi();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [console.log(drinks)]);
+  }, []);
 
-  // const ingredients = Object
-  //   .keys(meals).filter((key) => key.includes('strIngredient'));
-  // console.log(ingredients);
+  // const drinkIngredient = Object.keys(ingredient).filter((key) => key
+  //   .includes('strIngredient'));
+  console.log(drinks);
 
-  const ingredients = Object.keys(drink).filter((key) => key.includes('strIngredient'));
   return (
     <div>
       <Header />
@@ -38,26 +38,28 @@ export default function ExploreDrinkByIngredient() {
           loading ? <p>Loading...</p> : (
             <div>
               {
-                ingredients.map((ingredient, index) => (drink[ingredient] !== ''
-                  && (
-                    index <= NUMBER_ELEVEN && (
-                      <div
-                        data-testid={ `${index}-ingredient-card` }
-                        key={ ingredient.idDrink }
+                drinks.map((drink, index) => (
+                  index <= NUMBER_ELEVEN && (
+                    <div
+                      data-testid={ `${index}-ingredient-card` }
+                      key={ index }
+                    >
+                      <img
+                        data-testid={ `${index}-card-img` }
+                        src={ `https://www.thecocktaildb.com/images/ingredients/${drink.strIngredient1}-Small.png` }
+                        alt="recipes cards"
+                      />
+                      <Link
+                        data-testid={ `${index}-recipe-card` }
+                        to={ `/drinks/${drink.strIngredient1}` }
                       >
-                        <img
-                          data-testid={ `${index}-card-img` }
-                          src={ `https://www.themealdb.com/images/ingredients/${drink[ingredient]}-Small.png` }
-                          alt="recipes cards"
-                        />
-                        <Link
-                          data-testid={ `${index}-recipe-card` }
-                          to={ `/drinks/${drink[ingredient]}` }
-                        >
-                          <p data-testid={ `${index}-card-name` }>{drink[ingredient]}</p>
-                        </Link>
-                      </div>
-                    ))))
+                        <p data-testid={ `${index}-card-name` }>
+                          {drink.strIngredient1}
+
+                        </p>
+                      </Link>
+                    </div>
+                  )))
               }
             </div>
           )
