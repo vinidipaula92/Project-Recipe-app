80b82b5f6321391d2f670fd5fe6f223784dff1bf
Rafael Barbosa <rafaelbarbosa2k19@hotmail.com>
2022-05-04 17:06:06 -0300

    ok

--- src/pages/ProgressFood.jsx
+++ src/pages/ProgressFood.jsx
@@ -1,56 +1,75 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import ButtonShare from '../components/ButtonShare';
+import FavoriteButton from '../components/FavoriteButton';
 import '../css/progress.css';
 
 export default function ProgressFood() {
-  const [add, setAdd] = useState(false);
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    if (count === 0) {
-      setAdd(true);
-      setCount(1);
-    } else {
-      setAdd(false);
-      setCount(0);
-    }
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState({});
+  const [loading, setLoading] = useState(true);
+  // const [add, setAdd] = useState(false);
+  // const [count, setCount] = useState(0);
+
+  const getRecipeById = async () => {
+    const { meals } = await requestFoodRecipeById(id);
+    setRecipe(meals[0]);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    getRecipeById();
+  }, []);
+
+  const ingredients = Object.keys(recipe)
+    .filter((key) => key.includes('strIngredient'));
+
+  const measure = Object.keys(recipe)
+    .filter((key) => key.includes('strMeasure'));
+
+  // const handleClick = () => {
+  //   if (count === 0) {
+  //     setAdd(true);
+  //     setCount(1);
+  //   } else {
+  //     setAdd(false);
+  //     setCount(0);
+  //   }
+  // };
+
   return (
     <div>
-      <h1>Progress Food</h1>
-      <h3 data-testid="recipe-title">Titulo</h3>
-      <img
-        src="https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg"
-        alt="hamburger"
-      />
-      <ButtonShare />
-      <button
-        type="button"
-        data-testid="favorite-btn"
-      >
-        Favorite
-
-      </button>
-      <p data-testid="recipe-category">Category</p>
-      <label
-        htmlFor="ingredients"
-      >
-        Ingredients:
-        <input
-          type="checkbox"
-          id="ingredients"
-          data-testid="ingredient-step"
-          className={ add ? 'ingredient-step' : null }
-          onClick={ handleClick }
-        />
-      </label>
-      <p data-testid="instructions">Intructions</p>
-      <button
-        type="button"
-        data-testid="finish-recipe-btn"
-      >
-        Finish
-
-      </button>
+      {
+        loading ? <p>Loading...</p> : (
+          <div>
+            <div>
+              <img
+                data-testid="recipe-photo"
+                src={ recipe.strMealThumb }
+                alt={ recipe.strMeal }
+              />
+              <h1 data-testid="recipe-title">{recipe.strMeal}</h1>
+            </div>
+            <div>
+              <ButtonShare />
+              <FavoriteButton />
+            </div>
+            <p data-testid="recipe-category">{recipe.strCategory}</p>
+            {
+              ingredients.map((ingredient, index) => (recipe[ingredient] !== ''
+                && (
+                  <checkbox
+                    key={ ingredient }
+                    data-testid={ `${index}-ingredient-step` }
+                  >
+                    {`${recipe[measure[index]]} - ${recipe[ingredient]}`}
+                  </checkbox>
+                )
+              ))
+            }
+          </div>
+        )
+      }
     </div>
   );
 }
