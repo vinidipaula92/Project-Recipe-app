139050a8b904220835b2936308accf92256c499a
Pedrin <pedro515vieira@gmail.com>
2022-05-04 22:11:20 -0300

    Metade do requisito 61

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -20,14 +20,22 @@ export default function Favorite() {
       <span data-testid="page-title">Favorite Recipes</span>
       <div>
         {
-          favoriteItems && favoriteItems.map((element) => (
+          favoriteItems && favoriteItems.map((element, index) => (
             <div key={ element.id }>
-              <p>{ element.name }</p>
-              <p>{ element.nationality }</p>
-              <p>{ element.category }</p>
+              <p data-testid={ `${index}-horizontal-name` }>{ element.name }</p>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { element.nationality }
+                {' '}
+                -
+                {' '}
+                { element.category }
+              </p>
               <img
                 src={ element.image }
                 alt={ `foto do alimento com o id ${element.id}` }
+                data-testid={ `${index}-horizontal-image` }
               />
             </div>
           ))
